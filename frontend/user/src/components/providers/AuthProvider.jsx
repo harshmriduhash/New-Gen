5e247221b4b417effa8b0de4ff3890a5a9852f64
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-11-25 22:40:07 +0530

    read auth

--- frontend/user/src/components/providers/AuthProvider.jsx
+++ frontend/user/src/components/providers/AuthProvider.jsx
@@ -1,10 +1,6 @@
-import React from 'react';
-import { UserProvider } from '@/context/UserContext';
+import React from "react";
+import { UserProvider } from "@/context/UserContext";
 
 export default function AuthProvider({ children }) {
-    return (
-        <UserProvider>
-            {children}
-        </UserProvider>
-    )
+  return <UserProvider>{children}</UserProvider>;
 }
