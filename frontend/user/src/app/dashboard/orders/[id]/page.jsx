520cfbc36f5090cab5e4152e992ac4d5453f7cab
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-11-26 23:49:34 +0530

    setting up

--- frontend/user/src/app/dashboard/orders/[id]/page.jsx
+++ frontend/user/src/app/dashboard/orders/[id]/page.jsx
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function page({ params }) {
-  return (
-    <div>
-      {params.id}
-    </div>
-  )
+  return <div>{params.id}</div>;
 }
