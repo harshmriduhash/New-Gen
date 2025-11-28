3362c41fd9a20453dcfebab65a93922b55baeec9
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-11-28 13:22:07 +0530

    3d

--- frontend/user/src/components/system/3d-pin.jsx
+++ frontend/user/src/components/system/3d-pin.jsx
@@ -51,10 +51,7 @@ export const PinContainer = ({
   );
 };
 
-export const PinPerspective = ({
-  title,
-  href,
-}) => {
+export const PinPerspective = ({ title, href }) => {
   return (
     <motion.div className="pointer-events-none  w-96 h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
       <div className=" w-full h-full -mt-7 flex-none  inset-0">
