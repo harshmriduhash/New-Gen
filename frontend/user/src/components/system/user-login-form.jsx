92cb2edc20f0a486b24a5509090ce3f7ccd8c5da
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-11-28 13:27:27 +0530

    user

--- frontend/user/src/components/system/user-login-form.jsx
+++ frontend/user/src/components/system/user-login-form.jsx
@@ -44,7 +44,7 @@ export function UserLoginForm({ className, ...props }) {
       console.error("Login error:", error);
       toast.error(`Login error - ${error.response.data.message}`, {
         duration: 4000,
-        position: 'bottom-center',
+        position: "bottom-center",
       });
       setIsLoading(false);
     }
@@ -54,10 +54,13 @@ export function UserLoginForm({ className, ...props }) {
     setIsLoading(true);
     try {
       const result = await signInWithPopup(auth, googleProvider);
-      toast.success("Google Sign-In successful - Relax and stay calm, this may take a moment!", {
-        duration: 4000,
-        position: "bottom-center",
-      });
+      toast.success(
+        "Google Sign-In successful - Relax and stay calm, this may take a moment!",
+        {
+          duration: 4000,
+          position: "bottom-center",
+        }
+      );
       // Send login request to your backend
       const response = await axios.post(
         `${process.env.NEXT_PUBLIC_BASEURL}/auth/login`,
@@ -70,7 +73,7 @@ export function UserLoginForm({ className, ...props }) {
       toast.success("Google Sign-In successful", {
         duration: 4000,
         position: "bottom-center",
-      });      
+      });
       setIsLoading(false);
       router.push("/dashboard");
     } catch (error) {
@@ -90,7 +93,7 @@ export function UserLoginForm({ className, ...props }) {
         <div className="grid gap-2">
           <div className="grid gap-1">
             <Label className="sr-only" htmlFor="email">
-            email
+              email
             </Label>
             <Input
               id="email"
@@ -157,28 +160,31 @@ export function UserLoginForm({ className, ...props }) {
         disabled={isLoading}
         onClick={handleGoogleSignIn}
       >
-        <FcGoogle className="mr-2 h-4 w-4" /> {isLoading ? (
-              <svg
-                className="animate-spin h-4 w-4 mr-2 text-red-500"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
-            ) : "Google"}
+        <FcGoogle className="mr-2 h-4 w-4" />{" "}
+        {isLoading ? (
+          <svg
+            className="animate-spin h-4 w-4 mr-2 text-red-500"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        ) : (
+          "Google"
+        )}
       </Button>
     </div>
   );
